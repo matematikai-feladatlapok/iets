98d1ea90688d91cec018a31b3c7dc12b79b1088d
gycsaba <gyuris.csaba@vasvari.org>
2022-06-06 22:19:53 +0200

    Használható weboldalak

--- src/main/java/hu/iets/service/MathService.java
+++ src/main/java/hu/iets/service/MathService.java
@@ -4,6 +4,10 @@ import hu.iets.model.Task;
 
 public class MathService {
 
+    // https://prog.hu/cikkek/821/matematikai-kifejezesek-ertelmezese
+    // https://levelup.gitconnected.com/create-and-evaluate-simple-expression-tree-in-python-in-object-oriented-style-5eb27b6376c8
+    //https://medium.com/swlh/build-binary-expression-tree-in-python-36c04123e57b
+
     private Task task;
 
     public MathService(Task task) {
