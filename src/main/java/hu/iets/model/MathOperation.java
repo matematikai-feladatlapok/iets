190954a48dfe8d434f0b25f716a09c34071c5f16
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-06-09 14:15:21 +0200

    Enum típus metódusai

--- src/main/java/hu/iets/model/MathOperation.java
+++ src/main/java/hu/iets/model/MathOperation.java
@@ -1,3 +1,31 @@
 package hu.iets.model;
 
-public enum MathOperation {MUL, DIV, ADD, SUB}
+public enum MathOperation {
+    Mul, Div, Add, Sub;
+
+    public int getPrecedence () {
+        switch (this) {
+            case Mul:
+            case Div: return 2;
+            case Add:
+            case Sub: return 1;
+
+            default:
+                return -1;
+        }
+    }
+
+    public MathOperation getOperation(char operation) {
+        switch (operation) {
+            case '+': return MathOperation.Add;
+            case '-': return MathOperation.Sub;
+            case '*': return MathOperation.Mul;
+            case '/': return MathOperation.Div;
+
+            default:
+                return null;
+        }
+    }
+}
+
+
